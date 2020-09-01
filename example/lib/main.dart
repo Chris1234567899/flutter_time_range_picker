6b4c1cd6fc8294fea3232028274dde4b8f95ad94
Janez Stupar <janez@janezstupar.com>
2020-09-01 18:38:38 +0200

    Add inline widget to example app.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -232,6 +232,41 @@ class _MyHomePageState extends State<MyHomePage> {
             },
             child: Text("Background Widget"),
           ),
+          Divider(),
+          Text(
+            'As a regular widget:',
+            style: Theme.of(context).textTheme.headline6,
+          ),
+          Expanded(
+              child: Container(
+                  alignment: Alignment.topCenter,
+                  height: 400,
+                  width: 400,
+                  child: TimeRangePicker(
+                    hideButtons: true,
+                    hideTimes: true,
+                    paintingStyle: PaintingStyle.fill,
+                    backgroundColor: Colors.grey.withOpacity(0.2),
+                    labels: [
+                      ClockLabel.fromTime(
+                          time: TimeOfDay(hour: 7, minute: 0),
+                          text: "Start Work"),
+                      ClockLabel.fromTime(
+                          time: TimeOfDay(hour: 18, minute: 0), text: "Go Home")
+                    ],
+                    start: TimeOfDay(hour: 10, minute: 0),
+                    end: TimeOfDay(hour: 13, minute: 0),
+                    ticks: 8,
+                    strokeColor:
+                        Theme.of(context).primaryColor.withOpacity(0.5),
+                    ticksColor: Theme.of(context).primaryColor,
+                    labelOffset: 15,
+                    padding: 60,
+                    disabledTime: TimeRange(
+                        startTime: TimeOfDay(hour: 18, minute: 0),
+                        endTime: TimeOfDay(hour: 7, minute: 0)),
+                    disabledColor: Colors.red.withOpacity(0.5),
+                  )))
         ]),
       ),
     );
